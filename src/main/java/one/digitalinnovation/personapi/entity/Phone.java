456144ba7b3a31697e4f7303e29de59ab287ba3b
matpfreitas <matheuspfreitas@yahoo.com.br>
2021-09-24 15:29:20 -0300

    Inclusão do controller, entity e enums

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.*;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+}
