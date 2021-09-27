9ee6458be39301f1034bce6beaab8506f4cb7f8f
matpfreitas <matheuspfreitas@yahoo.com.br>
2021-09-27 14:28:02 -0300

    Implementado método POST e GET (All e por ID)

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,16 +1,46 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
+import one.digitalinnovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+
+        return personService.createPerson(personDTO);
+    }
+
     @GetMapping
-    public String getBook() {
-        return "API Test!";
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+
+
     }
 
 }
