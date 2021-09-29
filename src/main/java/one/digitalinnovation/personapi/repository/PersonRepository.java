60562edc1527f7230be9b6f337d483a6e8346236
matpfreitas <matheuspfreitas@yahoo.com.br>
2021-09-29 13:56:19 -0300

    Adicionado dependências do Swagger

--- src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
@@ -3,8 +3,4 @@ package one.digitalinnovation.personapi.repository;
 import one.digitalinnovation.personapi.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-
-
-}
+public interface PersonRepository extends JpaRepository<Person, Long> { }
